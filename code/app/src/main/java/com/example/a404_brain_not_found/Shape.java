6a1866681783d5a49936536561c130d3c0768472
daniil-zahoruiko <daniel.zagoruyko@gmail.com>
2025-01-30 20:12:49 -0700

    Add shape class

---
+++ code/app/src/main/java/com/example/a404_brain_not_found/Shape.java
@@ -0,0 +1,22 @@
+package com.example.a404_brain_not_found;
+
+public abstract class Shape {
+    protected int x;
+    protected int y;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
