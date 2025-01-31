3966884ae928c83fc7d1b28479b873fa49476ead
Brayden Bartelds <149531080+braydenmcb@users.noreply.github.com>
2025-01-30 20:20:09 -0700

    added Shape constructor and Star subclass

--- code/app/src/main/java/com/example/a404_brain_not_found/Shape.java
+++ code/app/src/main/java/com/example/a404_brain_not_found/Shape.java
@@ -4,6 +4,10 @@ public abstract class Shape {
     protected int x;
     protected int y;
 
+    public Shape(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
     public int getX() {
         return x;
     }
