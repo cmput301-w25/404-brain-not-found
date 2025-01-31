7970108bf866c4ee592e524854891f048523d38e
kashishgupta2911 <guptakashish2911@gmail.com>
2025-01-30 20:29:23 -0700

    blue color

--- code/app/src/main/java/com/example/a404_brain_not_found/Shape.java
+++ code/app/src/main/java/com/example/a404_brain_not_found/Shape.java
@@ -3,6 +3,7 @@ package com.example.a404_brain_not_found;
 public abstract class Shape {
     protected int x;
     protected int y;
+    String color = "blue";
 
     public int getX() {
         return x;
